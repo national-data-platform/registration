d853f2e5865cb87513bade5f2f6d8aaee8df15e0
iperezx <ismael.perezx@gmail.com>
2024-04-24 12:59:16 -0700

    Add pelican transfer jobs

--- src/controllers/handlers.go
+++ src/controllers/handlers.go
@@ -1,17 +1,21 @@
 package controllers
 
 import (
+	"context"
+	"log"
 	"net/http"
+	"net/url"
+	"os"
 	"registration/models"
 	"registration/utilities"
 
 	"github.com/gin-gonic/gin"
+	"github.com/pelicanplatform/pelican/client"
 	"gorm.io/gorm"
-	"io/ioutil"
 )
 
 type osdfPath struct {
-    Name     string  `json:"name"`
+	Name string `json:"name"`
 }
 
 // GET /download
@@ -19,13 +23,50 @@ type osdfPath struct {
 func GetFile(c *gin.Context) {
 	var newPath osdfPath
 	if err := c.BindJSON(&newPath); err != nil {
-        return
-    }
+		return
+	}
 	fileName := newPath.Name
-	inputFile, err := ioutil.ReadFile(fileName)
-    if err != nil {
-        panic(err)
-    }
+	inputFile, err := os.ReadFile(fileName)
+	if err != nil {
+		panic(err)
+	}
+	//Pelican object transfer
+	// ctx := context.Background()
+	te := client.NewTransferEngine(c)
+	defer func() {
+		if err := te.Shutdown(); err != nil {
+			log.Println("Failure when shutting down transfer engine:", err)
+		}
+	}()
+	var options []client.TransferOption
+	project := "" // Used for condor jobs
+	localDestination := "test.txt"
+	remoteObject := "/ospool/uc-shared/public/OSG-Staff/validation/test.txt"
+	remoteObjectUrl, err := url.Parse(remoteObject)
+	if err != nil {
+		log.Println("Failed to parse source URL:", err)
+	}
+	tc, err := te.NewClient(options...)
+	if err != nil {
+		log.Println("Failure when creating new client:", err)
+	}
+	tj, err := tc.NewTransferJob(context.Background(), remoteObjectUrl, localDestination, false, false, project)
+	if err != nil {
+		log.Println("Failure when creating new transfer job:", err)
+	}
+	err = tc.Submit(tj)
+	if err != nil {
+		log.Println("Failure when submitting job:", err)
+	}
+	transferResults, err := tc.Shutdown()
+	var downloaded int64 = 0
+	for _, result := range transferResults {
+		downloaded += result.TransferredBytes
+		if err == nil && result.Error != nil {
+			err = result.Error
+		}
+	}
+	log.Println("Downloaded results:", downloaded)
 	c.Header("Content-Disposition", "attachment; filename="+fileName)
 	c.Data(http.StatusOK, "application/data", inputFile)
 }
